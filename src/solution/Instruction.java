5b064fe9bfb8b9a4656438f488a13b692e72597b
nubela <nubela@gmail.com>
2011-08-23 11:47:52 +0800

    translation lib

--- src/solution/Instruction.java
+++ src/solution/Instruction.java
@@ -1,5 +1,58 @@
 package solution;
 
-public enum Instruction {
+public class Instruction {
+
+	/**
+	 * Get all Lift's velocity.
+	 * 
+	 * @return Translated String
+	 */
+	public String getAllLiftsVelocity() {
+		return "v";
+	}
+
+	/**
+	 * Inspects lift's position.
+	 * 
+	 * @param lift
+	 * @return Translated String
+	 */
+	public String inspectPositionOfLift(Lift lift) {
+		return "w " + lift.getElevatorId();
+	}
+
+	/**
+	 * Moves an elevator towards a certain direction, or stops it. -1,
+	 * Downwards; 0, Stops; 1. Upwards.
+	 * 
+	 * @param lift
+	 * @param direction
+	 * @return Translated String
+	 */
+	public String moveElevator(Lift lift, int direction) {
+		return "m " + lift.getElevatorId() + " " + direction;
+	}
+
+	/**
+	 * Opens or closes a door given the lift. 1 opens it, -1 closes.
+	 * 
+	 * @param lift
+	 * @param open
+	 * @return Translated String
+	 */
+	public String openDoor(Lift lift, int open) {
+		return "d " + lift.getElevatorId() + " " + open;
+	}
+
+	/**
+	 * Sets floor indicator of a lift.
+	 * 
+	 * @param lift
+	 * @param floorNumber
+	 * @return Translated String
+	 */
+	public String setLiftFloorIndicator(Lift lift, int floorNumber) {
+		return "s " + lift.getElevatorId() + " " + floorNumber;
+	}
 
 }
--- src/solution/Lift.java
+++ src/solution/Lift.java
@@ -2,4 +2,40 @@ package solution;
 
 public class Lift {
 
+	Status status;
+	Status position;
+	int elevatorId;
+
+	public Lift(int elevatorId) {
+		this.elevatorId = elevatorId;
+	}
+
+	public int getElevatorId() {
+		return elevatorId;
+	}
+
+	public Status getPosition() {
+		return position;
+	}
+
+	public Status getStatus() {
+		return status;
+	}
+
+	public void setElevatorId(int elevatorId) {
+		this.elevatorId = elevatorId;
+	}
+
+	public void setPosition(Status position) {
+		this.position = position;
+	}
+
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+	public static enum Status {
+		DOOR_OPEN, DOOR_CLOSE, IN_MOTION;
+	}
+
 }
